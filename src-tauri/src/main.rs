9825db55bb6d1fc8bb112d4a987d2387ea633237
NEOTSO <coding.tso@gmail.com>
2022-03-02 18:29:06 +0800

    feat: rename_folder, rename_files done

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,7 @@
+use std::fmt::Debug;
 use std::fs;
+use std::path::Path;
+use std::fs::DirEntry;
 
 // #![cfg_attr(
 //     all(not(debug_assertions), target_os = "windows"),
@@ -6,11 +9,55 @@ use std::fs;
 // )]
 
 #[tauri::command]
-fn rename_files(files: Vec<String>, separator: String) {
+fn rename_folder(folder: &str, separator: &str) {
+    let paths = fs::read_dir(folder).unwrap();
+    for path in paths {
+        let md = path.as_ref().unwrap().metadata().unwrap();
+        if md.is_dir() {
+            rename_folder(path.as_ref().unwrap().path().to_str().unwrap(), separator);
+        } else {
+            rename_files(vec![path.as_ref().unwrap().path().to_str().unwrap()], separator);
+        }
+    }
+    let mut ancestors = Path::new(folder).ancestors();
+    ancestors.next();
+    let parent_folder = ancestors.next().unwrap();
+    let file_name = Path::new(folder).file_name().unwrap().to_str().unwrap().to_string();
+    let chars: Vec<String> = file_name.chars().map(|x| x.to_string()).collect();
+    let new_file_name = chars.join(separator);
+    let new_path = parent_folder.join(new_file_name);
+    let result = fs::rename(folder, new_path);
+    let result = match result {
+        Ok(file) => {
+            file
+        },
+        Err(error) => {
+            panic!("Problem opening the file: {:?}", error)
+        },
+    };
+}
+
+#[tauri::command]
+fn rename_files(files: Vec<&str>, separator: &str) {
     println!("{}", separator);
     println!("{:?}", files);
-    for item in files {
-        println!("{}", item);
+    for file in files {
+        let path = Path::new(file);
+        let parent_folder = path.parent().unwrap();
+        let file_name = path.file_name().unwrap().to_str().unwrap();
+        let chars: Vec<String> = String::from(file_name).chars().map(|x| x.to_string()).collect();
+        let new_file_name = chars.join(separator);
+        let new_path = parent_folder.join(new_file_name);
+        let result = fs::rename(file, new_path);
+        let result = match result {
+            Ok(file) => {
+                file
+            },
+            Err(error) => {
+                panic!("Problem opening the file: {:?}", error)
+            },
+        };
+        // println!("{}", item);
     }
 }
 
@@ -35,7 +82,7 @@ fn main() {
     tauri::Builder::default()
         // This is where you pass in your commands
         // .invoke_handler(tauri::generate_handler![my_custom_command])
-        .invoke_handler(tauri::generate_handler![rename_files])
+        .invoke_handler(tauri::generate_handler![rename_folder, rename_files])
         .run(tauri::generate_context!())
         .expect("failed to run app")
 }
--- src/components/TheRename.vue
+++ src/components/TheRename.vue
@@ -3,18 +3,20 @@ import BaseFileList from "./BaseFileList.vue";
 import { invoke } from "@tauri-apps/api/tauri";
 import { open } from "@tauri-apps/api/dialog";
 import { emit, listen } from "@tauri-apps/api/event";
+import { readDir } from "@tauri-apps/api/fs";
 
 // With the Tauri global script, enabled when `tauri.conf.json > build > withGlobalTauri` is set to true:
 // const invoke = window.__TAURI__.invoke
 
 // Invoke the command
 
-const selectFolders = async () => {
-    const result = await open({ directory: true });
+const selectFolder = async () => {
+    const result = (await open({ directory: true })) as string;
     console.log(result);
     if (result) {
-        console.log("xxxx");
-        invoke("rename_files", { files: result });
+        console.log("####");
+        console.log(readDir)
+        invoke("rename_folder", { folder: result, separator: "#" });
     }
 };
 
@@ -23,7 +25,7 @@ const selectFiles = async () => {
     console.log(result);
     if (result) {
         console.log("xxxx");
-        invoke("rename_files", { files: result, separator: '#' });
+        invoke("rename_files", { files: result, separator: "#" });
     }
 };
 </script>
@@ -32,7 +34,7 @@ const selectFiles = async () => {
     <div>
         <div class="flex justify-between px-4 py-5">
             <div class="flex">
-                <div class="button" @click="selectFolders">打开文件夹</div>
+                <div class="button" @click="selectFolder">打开文件夹</div>
                 <div class="button ml-2" @click="selectFiles">打开文件</div>
                 <input class="border border-gray-300 outline-none px-2 ml-2" type="text" placeholder="请输入混淆文字" />
             </div>
