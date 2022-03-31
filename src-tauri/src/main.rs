e2705a5b115cb0eda08897c5992810f5bf051464
unknown <coding.tso@gmail.com>
2022-03-31 09:19:48 +0800

    feat: delete text from file name

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -21,14 +21,14 @@ struct Message<'a> {
 }
 
 #[tauri::command]
-fn rename(files: Vec<&str>, separator: &str, window: Window) {
+fn rename(files: Vec<&str>, keywords: &str, window: Window) {
     for file in files {
         let path = Path::new(file);
         let md = path.metadata().unwrap();
         if md.is_dir() {
-            let _result = rename_folder(path, separator);
+            let _result = rename_folder(path, keywords);
         } else {
-            let _result = rename_files(vec![&path], separator);
+            let _result = rename_files(vec![&path], keywords);
         }
     }
     let _result = window.emit(
@@ -39,14 +39,14 @@ fn rename(files: Vec<&str>, separator: &str, window: Window) {
     );
 }
 
-fn rename_folder(folder: &Path, separator: &str) -> std::io::Result<()> {
+fn rename_folder(folder: &Path, keywords: &str) -> std::io::Result<()> {
     for entry in fs::read_dir(folder)? {
         let dir = entry?;
         let md = dir.metadata()?;
         if md.is_dir() {
-            rename_folder(dir.path().as_path(), separator)?;
+            rename_folder(dir.path().as_path(), keywords)?;
         } else {
-            rename_files(vec![dir.path().as_path()], separator)?;
+            rename_files(vec![dir.path().as_path()], keywords)?;
         }
     }
     let mut ancestors = folder.ancestors();
@@ -57,13 +57,13 @@ fn rename_folder(folder: &Path, separator: &str) -> std::io::Result<()> {
         .chars()
         .map(|x| x.to_string())
         .collect();
-    let new_file_name = chars.join(separator);
+    let new_file_name = chars.join(keywords);
     let new_path = parent_folder.join(new_file_name);
     fs::rename(folder, new_path)?;
     Ok(())
 }
 
-fn rename_files(files: Vec<&Path>, separator: &str) -> std::io::Result<()> {
+fn rename_files(files: Vec<&Path>, keywords: &str) -> std::io::Result<()> {
     for path in files {
         let parent_folder = path.parent().unwrap();
         let file_name = path.file_stem().unwrap();
@@ -72,7 +72,7 @@ fn rename_files(files: Vec<&Path>, separator: &str) -> std::io::Result<()> {
             .chars()
             .map(|x| x.to_string())
             .collect();
-        let new_file_name = chars.join(separator) + "." + file_ext.to_str().unwrap();
+        let new_file_name = chars.join(keywords) + "." + file_ext.to_str().unwrap();
         let new_path = parent_folder.join(new_file_name);
         fs::rename(path, new_path)?;
     }
@@ -80,14 +80,14 @@ fn rename_files(files: Vec<&Path>, separator: &str) -> std::io::Result<()> {
 }
 
 #[tauri::command]
-fn restore(files: Vec<&str>, separator: &str, window: Window) {
+fn restore(files: Vec<&str>, keywords: &str, window: Window) {
     for file in files {
         let path = Path::new(file);
         let md = path.metadata().unwrap();
         if md.is_dir() {
-            let _result = restore_folder(path, separator);
+            let _result = restore_folder(path, keywords);
         } else {
-            let _result = restore_files(vec![path], separator);
+            let _result = restore_files(vec![path], keywords);
         }
     }
     let _result = window.emit(
@@ -98,14 +98,14 @@ fn restore(files: Vec<&str>, separator: &str, window: Window) {
     );
 }
 
-fn restore_folder(folder: &Path, separator: &str) -> std::io::Result<()> {
+fn restore_folder(folder: &Path, keywords: &str) -> std::io::Result<()> {
     for entry in fs::read_dir(folder)? {
         let dir = entry?;
         let md = dir.metadata()?;
         if md.is_dir() {
-            restore_folder(dir.path().as_path(), separator)?;
+            restore_folder(dir.path().as_path(), keywords)?;
         } else {
-            restore_files(vec![dir.path().as_path()], separator)?;
+            restore_files(vec![dir.path().as_path()], keywords)?;
         }
     }
     let parent_folder = folder.parent().unwrap();
@@ -113,7 +113,7 @@ fn restore_folder(folder: &Path, separator: &str) -> std::io::Result<()> {
     let chars: Vec<String> = String::from(file_name.to_str().unwrap())
         .chars()
         .map(|x| x.to_string())
-        .filter(|x| x != separator)
+        .filter(|x| x != keywords)
         .collect();
     let new_file_name = chars.join("");
     let new_path = parent_folder.join(new_file_name);
@@ -121,7 +121,7 @@ fn restore_folder(folder: &Path, separator: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-fn restore_files(files: Vec<&Path>, separator: &str) -> std::io::Result<()> {
+fn restore_files(files: Vec<&Path>, keywords: &str) -> std::io::Result<()> {
     for path in files {
         let parent_folder = path.parent().unwrap();
         let file_name = path.file_stem().unwrap();
@@ -129,9 +129,57 @@ fn restore_files(files: Vec<&Path>, separator: &str) -> std::io::Result<()> {
         let chars: Vec<String> = String::from(file_name.to_str().unwrap())
             .chars()
             .map(|x| x.to_string())
-            .filter(|x| x != separator)
+            .filter(|x| x != keywords)
             .collect();
         let new_file_name = chars.join("") + "." + file_ext.to_str().unwrap();
+        let new_path = parent_folder.join(new_file_name);
+        fs::rename(path, new_path)?;
+    }
+    Ok(())
+}
+
+#[tauri::command]
+fn delete_str(files: Vec<&str>, keywords: &str, window: Window) {
+    for file in files {
+        let path = Path::new(file);
+        let md = path.metadata().unwrap();
+        if md.is_dir() {
+            let _result = delete_folder_str(path, keywords);
+        } else {
+            let _result = delete_files_str(vec![path], keywords);
+        }
+    }
+    let _result = window.emit(
+        "done",
+        Message {
+            message: "删除完成！",
+        },
+    );
+}
+
+fn delete_folder_str(folder: &Path, keywords: &str) -> std::io::Result<()> {
+    for entry in fs::read_dir(folder)? {
+        let dir = entry?;
+        let md = dir.metadata()?;
+        if md.is_dir() {
+            delete_folder_str(dir.path().as_path(), keywords)?;
+        } else {
+            delete_files_str(vec![dir.path().as_path()], keywords)?;
+        }
+    }
+    let parent_folder = folder.parent().unwrap();
+    let file_name = folder.file_name().unwrap();
+    let new_folder_name = str::replace(file_name.to_str().unwrap(), keywords, "");
+    let new_path = parent_folder.join(new_folder_name);
+    fs::rename(folder, new_path)?;
+    Ok(())
+}
+
+fn delete_files_str(files: Vec<&Path>, keywords: &str) -> std::io::Result<()> {
+    for path in files {
+        let parent_folder = path.parent().unwrap();
+        let file_name = path.file_name().unwrap();
+        let new_file_name = str::replace(file_name.to_str().unwrap(), keywords, "");
         let new_path = parent_folder.join(new_file_name);
         fs::rename(path, new_path)?;
     }
@@ -171,7 +219,7 @@ fn main() {
             }
             _ => {}
         })
-        .invoke_handler(tauri::generate_handler![rename, restore,])
+        .invoke_handler(tauri::generate_handler![rename, restore, delete_str])
         .run(tauri::generate_context!())
         .expect("failed to run app")
 }
--- src/components/TheRename.vue
+++ src/components/TheRename.vue
@@ -1,47 +1,53 @@
 <script setup lang="ts">
 import { invoke } from "@tauri-apps/api/tauri";
-import { emit, listen, Event } from "@tauri-apps/api/event";
+import { emit, listen, Event as TauriEvent } from "@tauri-apps/api/event";
 import { ref, Ref } from "vue";
 import BaseDrag from "../components/BaseDrag.vue";
 
-const separator = ref("❤");
-const isDragOver = ref(false);
+const mode: Ref<string> = ref("字符分隔");
+const keywords: Ref<string> = ref("❤");
+const isDragOver: Ref<boolean> = ref(false);
 const selectedFiles: Ref<string[]> = ref([]);
-const message = ref("");
+const message: Ref<string> = ref("");
+
+const changeMode = (e: Event) => {
+    console.log((e.target as HTMLInputElement).value);
+    mode.value = (e.target as HTMLInputElement).value;
+};
 
 const restore = () => {
-    invoke("restore", { files: selectedFiles.value, separator: separator.value });
+    invoke("restore", { files: selectedFiles.value, keywords: keywords.value });
 };
 
 const rename = () => {
-    invoke("rename", { files: selectedFiles.value, separator: separator.value });
+    invoke("rename", { files: selectedFiles.value, keywords: keywords.value });
 };
 
+const deleteStr = () => {
+    invoke("delete_str", { files: selectedFiles.value, keywords: keywords.value });
+}
+
 interface IPayloadFiles {
-    files: string[],
+    files: string[];
 }
 
 interface IPayloadMessage {
-    message: string,
+    message: string;
 }
 
-listen("files-selected", (event: Event<IPayloadFiles>) => {
-    console.log(event);
+listen("files-selected", (event: TauriEvent<IPayloadFiles>) => {
     selectedFiles.value = event.payload.files;
 });
 
-listen("done", (event: Event<IPayloadMessage>) => {
-    // selectedFiles.value = [event.payload.files];
+listen("done", (event: TauriEvent<IPayloadMessage>) => {
     message.value = event.payload.message;
 });
 
-listen("tauri://file-drop-hover", (event) => {
+listen("tauri://file-drop-hover", (event: TauriEvent<null>) => {
     isDragOver.value = true;
 });
 
-listen("tauri://file-drop", (event: Event<string[]>) => {
-    console.log(event);
-    console.log(event.payload);
+listen("tauri://file-drop", (event: TauriEvent<string[]>) => {
     message.value = "";
     isDragOver.value = false;
     selectedFiles.value = event.payload;
@@ -52,11 +58,20 @@ listen("tauri://file-drop", (event: Event<string[]>) => {
     <div class="px-4 py-5 flex flex-col h-full">
         <div class="flex justify-between">
             <div class="flex">
-                <input class="border border-gray-300 outline-none px-2" type="text" placeholder="请输入混淆文字" v-model="separator" />
+                <select class="outline-none" @change="changeMode">
+                    <option value="字符分隔">字符分隔</option>
+                    <option value="删除文字">删除文字</option>
+                </select>
             </div>
             <div class="flex">
+                <input class="border border-gray-300 outline-none px-2" type="text" placeholder="请输入混淆文字" v-model="keywords" />
+            </div>
+            <div v-if="mode === '字符分隔'" class="flex">
                 <div class="button" @click="restore">还原</div>
                 <div class="button ml-2" @click="rename">混淆</div>
+            </div>
+            <div v-if="mode === '删除文字'" class="flex">
+                <div class="button" @click="deleteStr">确认删除</div>
             </div>
         </div>
         <p v-if="message">{{ message }}</p>
